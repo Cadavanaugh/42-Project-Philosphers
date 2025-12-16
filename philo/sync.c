e51ed340e424b57ed7ff6bd0aeba8e87b953859d
jode-cas <jovanecastro12@gmail.com>
2025-12-16 17:55:46 -0300

    FEAT: utils and sync functions

---
+++ philo/sync.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sync.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 14:20:16 by jode-cas          #+#    #+#             */
+/*   Updated: 2025/12/15 18:11:55 by jode-cas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+void	wait_all_threads(t_table *table)
+{
+	while (get_long(&table->table_mutex,
+			&table->n_threads_running) < table->n_philos)
+		;
+	set_long(&table->table_mutex, &table->start_time, gettime());
+}
---
+++ philo/utils.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jode-cas <jode-cas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 13:36:23 by jode-cas          #+#    #+#             */
+/*   Updated: 2025/12/15 18:13:35 by jode-cas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+void	set_long(pthread_mutex_t *mutex, unsigned long *attr, unsigned long value)
+{
+	pthread_mutex_lock(mutex);
+	*attr = value;
+	pthread_mutex_unlock(mutex);
+}
+
+void	set_char(pthread_mutex_t *mutex, char *attr, char value)
+{
+	pthread_mutex_lock(mutex);
+	*attr = value;
+	pthread_mutex_unlock(mutex);
+}
+
+unsigned long	get_long(pthread_mutex_t *mutex, unsigned long *attr)
+{
+	unsigned long	value;
+
+	pthread_mutex_lock(mutex);
+	value = *attr;
+	pthread_mutex_unlock(mutex);
+	return (value);
+}
+
+char	get_char(pthread_mutex_t *mutex, char *attr)
+{
+	char	value;
+
+	pthread_mutex_lock(mutex);
+	value = *attr;
+	pthread_mutex_unlock(mutex);
+	return (value);
+}
+
+char	dinner_has_finished(t_table *table)
+{
+	return (get_char(&table->table_mutex,
+			&table->is_dinner_finished));
+}
